1b12a55a7f803af872c9cff6eb6be894e3a3307b
instinctualjealousy <odelladam@outlook.com>
2023-02-06 11:03:21 -0600

    Hopefully fixed feedreader.js (opted to bake URL in)

--- js/feedreader.js
+++ js/feedreader.js
@@ -1,2 +1,17 @@
-let API="https://api.rss2json.com/v1/api.json?rss_url=";let userFeedURLs=localStorage.getItem('userFeedURLs');userFeedURLs=userFeedURLs.split(",")
-userFeedURLs.forEach(userUrl=>{$.ajax({type:'GET',url:API+userUrl,dataType:'jsonp',success:function(data){console.log(data);data.items.forEach(item=>{var content=document.getElementById('content');var newItem="";newItem+="<div class=\"container\" id=\"item\"><a href=\""+item.link+"\"><h5><span style=\"font-weight: bold;\">"+item.title+"</span> <span style=\"color: black;\">("+item.pubDate+")</span></h5></a>";newItem+=item.description+"<br><br></div>";content.insertAdjacentHTML('beforeend',newItem);});}});});
+{
+    $.ajax({
+        type: 'GET',
+        url: "https://api.rss2json.com/v1/api.json?rss_url=https://revoltcars.tk/updates.rss",
+        dataType: 'jsonp',
+        success: function (data) {
+            console.log(data);
+            data.items.forEach(item => {
+                var content = document.getElementById('content');
+                var newItem = "";
+                newItem += "<div class=\"container\" id=\"item\"><a href=\"" + item.link + "\"><h5><span style=\"font-weight: bold;\">" + item.title + "</span> <span style=\"color: black;\">(" + item.pubDate + ")</span></h5></a>";
+                newItem += item.description + "<br><br></div>";
+                content.insertAdjacentHTML('beforeend', newItem);
+            });
+        }
+    });
+};
