0490445be22d3757bf89a82937658950198a0b39
instinctualjealousy <odelladam@outlook.com>
2023-02-07 11:31:53 -0600

    Minor refactoring of CSS usage

--- js/splashcycle.js
+++ js/splashcycle.js
@@ -1,1 +1,1 @@
-document.write('<style>#landsplash{background-image:url(\'assets/landing/splash'+Math.round(Math.random()*5+1)+'.jpg\'); background-repeat: no-repeat;}</style>');
+document.write('<style>#landsplash{background-image: url(\'assets/landing/splash' + Math.round(Math.random() * 5 + 1) + '.jpg\'); background-repeat: no-repeat;}</style>');
