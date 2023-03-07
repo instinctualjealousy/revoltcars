424a9f06d2c234782000f286a96e589e8188ef3c
instinctualjealousy <odelladam@outlook.com>
2023-03-06 21:27:05 -0600

    RSS JSON new generation method (again) test

--- js/feedreader.js
+++ js/feedreader.js
@@ -7,7 +7,7 @@
             console.log(data);
             data.items.forEach(item => {
                 var content = document.getElementById('content');
-                var sOriginalDate = item.publishedParsed;
+                var sOriginalDate = item.pubDate;
                 var oDate = new Date(sOriginalDate);
                 var iMonth = oDate.getMonth() + 1;
                 var iDay = oDate.getDate();
